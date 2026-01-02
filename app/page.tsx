f32f403c97cfe98fb2bb2efc517b36fd809921d1
Indrawan-maker <indrawanismail0@gmail.com>
2026-01-02 15:51:17 +0800

    feat: costumer kami & footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Hero } from "@/components/Hero"
 import { PaketHarga } from "@/components/PaketHarga"
 import { Portfolio } from "@/components/Portfolio"
 import { Customer } from "@/components/Customer"
+import { Footer } from "@/components/Footer"
 export default function Home() {
   return (
 <>
@@ -11,6 +12,7 @@ export default function Home() {
 <PaketHarga />
 <Portfolio />
 <Customer />
+<Footer />
 </>
   );
 }
