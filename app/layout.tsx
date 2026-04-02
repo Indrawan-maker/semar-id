4825c9ce17fccbeb65346f4a22eb4e7bcd65cbae
Muhammad Indrawan (Indrawan) <indrawanismail0@gmail.com>
2026-04-03 07:16:14 +0800

    fic path css salah

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals";
+import "./globals.css";
 import WhatsAppButton from "@/components/WhatsAppButton";
 import Script from "next/script";
 
